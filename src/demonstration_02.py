4e2036d3e332c65a1511446787186e8d1a06ec26
Tucker <leachtucker@gmail.com>
2021-02-11 16:51:43 -0800

    solved

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -16,5 +16,13 @@ Notes:
 
 
 def add_indexes(numbers):
-    # Your code here
+    newLst = []
+    i = 0
+    while i < len(numbers):
+        newLst.append(numbers[i] + i)
+        i += 1
+    return newLst
 
+print(add_indexes([0, 0, 0, 0, 0]))
+print(add_indexes([1, 2, 3, 4, 5]))
+print(add_indexes([5, 4, 3, 2, 1]))
